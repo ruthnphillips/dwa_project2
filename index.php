0b571f946d7709d3176a6821351a400810594afb
Ruth Phillips <ruthnphillips@gmail.com>
2017-09-17 02:33:18 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<title>Handout Diner</title>
+	<meta charset='utf-8'>
+
+</head>
+<body>
+
+	<?php echo "Handout";?>
+
+</body>
+</html>
