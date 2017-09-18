7b67eb3da5a200a81beb3fcf7386e8bbc66d1809
Ruth Phillips <ruthnphillips@gmail.com>
2017-09-17 22:04:36 -0400

    Modified index.html to include styling for forms

--- index.php
+++ index.php
@@ -1,43 +1,49 @@
-<?php require('getHandout.php'); ?>
+<!--<?php require('getHandout.php'); ?> -->
 
 <!DOCTYPE html>
 <html>
 
 <head>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 	<title>Handout Diner</title>
 	<meta charset='utf-8'>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
 
-	<?php echo "Handout";?>
+	<h1> <a href'/'> Handout Diner </a></h1>
 
-	<form method='GET' action='getHandout.php'>
+	<form method='GET'>
 
-		<!-- drop menu to select program -->
-		<select>
-		  <option value="computerScience">Computer Science</option>
-		  <option value="law">Law</option>
-		  <option value="businessAdmin">Business Administration</option>
-		</select>
-		
-		<!-- radio button to select semester -->
-		<input type="radio" name="semester" value="fall"> Fall
-  		<input type="radio" name="semester" value="spring"> Spring
+		<div class="form-group">
+			<!-- drop menu to select program -->
+			<h4>Select your program</h4>
+			<select class="form-control" name = "program">
+			  <option value="Computer Science">Computer Science</option>
+			  <option value="Law">Law</option>
+			  <option value="Business Administration">Business Administration</option>
+			</select>
+		</div>
 
-		<!-- text input to enter student ID -->
-		<label>Student ID:
-			<input type='text' id='studentID' name='studentID'>
-		</label>
-		<!-- submit button -->
-		 <input type='submit' value='Search'>
+		<div class="form-group">
+			<!-- radio button to select semester -->
+			<h4>Select your semester</h4>
+			<label class="radio-inline"><input type="radio" name="semester"> Fall</label>
+			<label class="radio-inline"><input type="radio" name="semester"> Spring</label>
+		</div>
+
+		<div class="form-group">
+			<!-- email input to enter email address -->
+			<h4>Enter your email</h4>
+			<input type='email' class='form-control' id='inputEmail' name='email'>
+		</div>
+
+			<!-- submit button -->
+			 <input type='submit' class='btn btn-primary btn-small' value='email Handout'>
 
 	</form>
+	
 </body>
 </html>
